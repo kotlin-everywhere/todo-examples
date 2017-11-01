b7b016d9497c7a7f80c67183ec09027793b3d4dd
Park Sung Min <vamf12@gmail.com>
2017-11-01 17:39:15 +0900

    remove useless configurations.

--- 01_browser/webpack/webpack.config.js
+++ 01_browser/webpack/webpack.config.js
@@ -13,32 +13,7 @@ module.exports = {
         path: __dirname + "/build",
         filename: "bundle.js"
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-                loader: 'url-loader',
-                options: {limit: 10000}
-            },
-            {
-                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-                loader: 'file-loader'
-            }
-        ]
-    },
-    devServer: {
-        hot: true,
-        proxy: {
-            "/api": {
-                target: "http://localhost:5000",
-                pathRewrite: {"^/api": ""}
-            }
-        }
-    },
+    devServer: { hot: true },
     plugins: [
         new HtmlWebpackPlugin({template: './index.ejs'}),
         new webpack.HotModuleReplacementPlugin()
